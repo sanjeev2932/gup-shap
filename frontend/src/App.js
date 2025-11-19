adcf84d4032d8dc962bac3ab79ab3c219e57e25d
sanjeev2932 <sanjeev2932005@gmail.com>
2025-11-20 03:28:16 +0530

    Improved VideoMeet + controls + auth UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 // frontend/src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 import Authentication from "./pages/Authentication";
 import JoinExisting from "./pages/JoinExisting";
 import VideoMeet from "./pages/VideoMeet";
-
-/*
-  Main router. NOTE:
-  - / => landing
-  - /auth => login/register
-  - /join => join existing page
-  - /:roomId => dynamic room page (VideoMeet)
-*/
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
         <Route path="/join" element={<JoinExisting />} />
         <Route path="/:roomId" element={<VideoMeet />} />
