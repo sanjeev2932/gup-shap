b503d439bdcab00ac061743551e2426e919a302e
sanjeev2932 <sanjeev2932005@gmail.com>
2025-11-20 01:08:28 +0530

    changed

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,14 +1,14 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const userScheme = new Schema(
-    {
-        name: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        token: { type: String }
-    }
-)
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userScheme);
+const User = mongoose.model("User", userSchema);
 
-export { User };
+export default User;
