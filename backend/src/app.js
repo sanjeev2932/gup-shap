98b371bcb45e1b2ccb1a2a36e017ab24bad4f527
sanjeev2932 <sanjeev2932005@gmail.com>
2025-11-21 17:12:10 +0530

    asd

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -9,13 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// CONNECT MONGODB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 // ROUTES
 app.use("/api/users", userRoutes);
 
-// Root test
+// ROOT TEST
 app.get("/", (req, res) => {
   res.json({ message: "Backend running 🚀" });
 });
 
-// EXPORT
 export default app;
