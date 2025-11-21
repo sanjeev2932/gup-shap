98ca551d5df433c6183602a563a9badc5046f98b
sanjeev2932 <sanjeev2932005@gmail.com>
2025-11-21 18:26:50 +0530

    asd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
+// src/App.js
+
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 import Landing from "./pages/landing";
 import Authentication from "./pages/authentication.jsx";
@@ -10,15 +13,17 @@ import History from "./pages/history.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/joinExisting" element={<JoinExisting />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/meet/:roomId" element={<VideoMeet />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/joinExisting" element={<JoinExisting />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/meet/:roomId" element={<VideoMeet />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
